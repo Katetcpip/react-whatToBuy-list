9c5bd7c6e7b3d92e8b49c8f46172cc42cbf207a3
Katetcpip <katetcpip@yabdex.ru>
2023-02-10 01:01:16 +0300

    img alt="" add

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,7 +6,7 @@ function Card(props:any){
     return(
         <div className='rounded-xl shadow-lg bg-slate-100 hover:bg-slate-200 flex flex-row lg:w-2/5 lg:py-6 lg:px-4 lg:mb-8 lg:mt-8 lg:m-auto m-4 box-border items-center justify-between'>     
         <div className='lg:text-2xl text-lg font-semibold ml-4 flex flex-row gap-4 items-center'>
-        <img className='lg:w-25 lg:h-20 w-15 h-10' src={item.img}></img>
+        <img alt="" className='lg:w-25 lg:h-20 w-15 h-10' src={item.img}></img>
           {item.name}</div>
         <div className="flex flex-row justify-end">
           <ButtonCh  item={item} changeAdd={changeAdd}/>
